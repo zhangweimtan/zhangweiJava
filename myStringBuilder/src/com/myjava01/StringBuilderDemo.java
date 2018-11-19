500e2ab63b784ef760d473ac1b233330922365eb
zhangweimtan <zhangweim@hotmail.com>
2018-11-19 16:53:27 +0800

    by zhangwei 2018-11-19

---
+++ myStringBuilder/src/com/myjava01/StringBuilderDemo.java
@@ -0,0 +1,16 @@
+package com.myjava01;
+/*
+    StringBuilder:是一个可变的字符串。字符串缓冲区类。
+    String 和StringBuilder的区别：
+        String 的内容是固定的；
+        StringBuilder是可变的
+    构造方法：
+        StringBuilder();
+ */
+public class StringBuilderDemo {
+    public static void main(String[] args) {
+//        创建对象
+        StringBuilder sb = new StringBuilder();
+        System.out.println("sb:"+sb);
+    }
+}
